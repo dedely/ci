a387605d4daf6fce3dc0393aaed00177f044790d
Adel <57968261+dedely@users.noreply.github.com>
2021-06-11 12:13:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require ('express');
 const app = express();
 
 app.get('/', (req, res) =>{
-    res.send("Finally Working...");
+    res.send("Finally Working but does it restart?");
 });
 
 const server = app.listen(process.env.PORT, process.env.IP, () => console.log('Server ready\nPid is : ' + process.pid + 'port ' + process.env.PORT));
