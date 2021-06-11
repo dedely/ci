816a19198d55a3cc50c2de10e19bbf2edc120db5
Adel <57968261+dedely@users.noreply.github.com>
2021-06-11 12:10:21 +0200

    update deploy script and app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require ('express');
 const app = express();
 
 app.get('/', (req, res) =>{
-    res.send("Almost Working...");
+    res.send("Finally Working...");
 });
 
 const server = app.listen(process.env.PORT, process.env.IP, () => console.log('Server ready\nPid is : ' + process.pid + 'port ' + process.env.PORT));
